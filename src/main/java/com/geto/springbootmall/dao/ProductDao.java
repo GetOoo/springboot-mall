b8a21c07e79a530d4ce9c01b56b5b241b22364b9
SKY-20170618RRR\Administrator <owen.ast328@gmail.com>
2022-10-05 00:06:05 +0800

    商品功能 - Get 所有商品 in a list

--- src/main/java/com/geto/springbootmall/dao/ProductDao.java
+++ src/main/java/com/geto/springbootmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.geto.springbootmall.dao;
 import com.geto.springbootmall.dto.ProductRequest;
 import com.geto.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/geto/springbootmall/service/ProductService.java
+++ src/main/java/com/geto/springbootmall/service/ProductService.java
@@ -3,7 +3,12 @@ package com.geto.springbootmall.service;
 import com.geto.springbootmall.dto.ProductRequest;
 import com.geto.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
