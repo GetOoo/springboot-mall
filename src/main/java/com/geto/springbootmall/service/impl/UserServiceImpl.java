41e33e58e99dbc4f0bfe1a1ccb7a8323e686a172
SKY-20170618RRR\Administrator <owen.ast328@gmail.com>
2022-10-05 18:53:51 +0800

    賬號功能 - Register 隱藏response返回Json中的password

--- src/main/java/com/geto/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/geto/springbootmall/service/impl/UserServiceImpl.java
@@ -4,17 +4,30 @@ import com.geto.springbootmall.dao.UserDao;
 import com.geto.springbootmall.dto.UserRegisterRequest;
 import com.geto.springbootmall.model.User;
 import com.geto.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+        //check email exist
+        if (user != null) {
+            log.warn("This email: \"{}\" had been register", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        // create user
         return userDao.createUser(userRegisterRequest);
     }
 
